54e18c5f4dce5425946ff9c034618f92e7eec16c
Eric Shaw <ericshawmail@gmail.com>
2021-04-17 19:10:10 -0400

    Add department functional, add role in progress

--- index.js
+++ index.js
@@ -30,7 +30,9 @@ const init = () => {
         'Add departments',
         'Add roles',
         'Add employees',
-        'Update employee roles'
+        'Delete departments',
+        'Update employee roles',
+        'Finish'
       ],
     })
     .then(answer => {
@@ -43,6 +45,18 @@ const init = () => {
                 break;
             case 'View employees':
                 viewEmployees();
+            case 'Add departments':
+                addDepartments();
+                break;
+            case 'Add roles':
+                addRoles();
+                break;
+            case 'Delete departments':
+                deleteDepartment();
+                break;
+            case 'Finish':
+                end();
+                break;
             default:
                 break;
         } 
@@ -52,38 +66,98 @@ const init = () => {
 }
 
 const viewDepartment = () => {
-    connection.query('SELECT * FROM department', (err, res) => {
+    connection.query('SELECT id AS ID, name AS Department FROM department;', (err, res) => {
         if (err) throw err;
         console.table(res);
+        init();
     })
 };
 
 const viewRoles = () => {
-    connection.query('SELECT * FROM role', (err, res) => {
+    connection.query('SELECT * FROM role;', (err, res) => {
         if (err) throw err;
         console.table(res);
+        init();
     })
 }
 
 const viewEmployees = () => {
-    connection.query('SELECT * FROM employee', (err, res) => {
+    connection.query('SELECT * FROM employee;', (err, res) => {
         if (err) throw err;
         console.table(res);
+        init();
     })
 }
 
 const addDepartments = () => {
-
+    inquirer
+    .prompt({
+        type: 'input',
+        name: 'addDpt',
+        message: 'Which department do you want to add?'
+    })
+    .then(answer => {
+        connection.query(
+            `INSERT INTO department (name)
+            VALUES ('${answer.addDpt}');`,
+            (err, res) => {
+                if (err) throw err;
+                init();
+            }
+        )
+    })
 }
 
 const addRoles = () => {
-
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the new title?'
+        },
+        {
+            type: 'input',
+            name: 'salary',
+            message: 'What is the new salary?'
+        },
+        {
+            type: 'input',
+            name: 'deptId',
+            message: 'What is the new department ID?'
+        }
+    ])
+    .then(answer => {
+        connection.query(
+            `INSERT INTO role (title, salary, department_id)
+            VALUES ('${answer.title}', '${answer.salary}', '${answer.deptId}');
+        `, (err, res) => {
+            if (err) throw err;
+            init();
+        }
+        )
+        
+    })
+    
 }
 
 const addEmployees = () => {
 
 }
 
+const deleteDepartment = () => {
+    connection.query('DELETE FROM department WHERE id > 5;',
+        (err, res) => {
+            if (err) throw err;
+            init();
+        }
+    )
+}
+
 const updateEmployeeRoles = () => {
 
+}
+
+const end = () => {
+    connection.end();
 }
